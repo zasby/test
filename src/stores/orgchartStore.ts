3e41a1fe7c7ad9fb3f306dd361693782a45ccf66
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-02-21 17:52:42 +0300

    update

--- src/stores/orgchartStore.ts
+++ src/stores/orgchartStore.ts
@@ -80,6 +80,7 @@ export default class OrgchartStore {
       this.currentOrgchart?.resultName && this.currentOrgchart.resultName.trim().length > 0
         ? this.currentOrgchart.resultName
         : t("ui:placeholder.glossary.r");
+    console.log('this.currentOrgchartResultName', this.currentOrgchartResultName);
   }
 
   getOrgchartResultNameByOrgchartId(orgchartId: number) {
