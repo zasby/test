c6ecb694f43f2fb5b1eebc007a1b8a76f0346a26
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-02-21 14:58:17 +0300

    update

--- src/plugins/fullCalendar/fullCalendarPluginDayjsTimeZone/index.ts
+++
@@ -1,7 +0,0 @@
-import { createPlugin, PluginDef } from "@fullcalendar/core";
-import { DayjsNamedTimeZone } from "./DayjsNamedTimeZone";
-
-export default createPlugin({
-  name: "fullCalendarPluginDayjsTimeZone",
-  namedTimeZonedImpl: DayjsNamedTimeZone,
-}) as PluginDef;
