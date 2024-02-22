dfe5887e4a5b9979162b69fe412dc152a1a1fd51
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-02-22 16:52:14 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { useRootStore } from "./hooks/useRootStore";
 export { useIsMobile } from "./hooks/useIsMobile";
 export { useDebounce } from "./hooks/useDebounce";
 export { usePaging, usePagingWithController } from "./hooks/usePaging";
-export { useDateHelpers } from "./useDateHelpers";
+export { useDateHelpers } from "./hooks/useDateHelpers";
 export { useNotifier } from "./hooks/useNotifier";
