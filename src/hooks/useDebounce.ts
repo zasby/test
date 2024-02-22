dd2da04f92b4b08ee624432aebabcafdeef7a42c
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-02-22 12:53:38 +0300

    update

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const useDebounce(value: any, timeout: undefined | number = 400) => {
+export const useDebounce = (value: any, timeout: undefined | number = 400) => {
   const [state, setState] = useState(value);
 
   useEffect(() => {
