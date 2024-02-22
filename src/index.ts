56e5ebfda748abad699623b2af93ba463c99fa7b
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-02-22 12:48:07 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { useAuth } from './hooks/loginForm/useAuth';
 export { useRootStore } from "./hooks/useRootStore";
 export { useIsMobile } from "./hooks/useIsMobile";
+export { useDebounce } from "./hooks/useDebounce";
+export { usePaging } from "./hooks/usePaging";
 // export { useNotifier } from "./hooks/useNotifier";
