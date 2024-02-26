5e2f26355da37dbcec2157b084a55dc1e1f4efcf
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-02-26 15:14:32 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,53 @@
-import {RootStoreProvider, StoreContext} from "./contexts/storeContext";
-import {GlobalAuthorizedContext, GlobalAuthorizedContextProvider} from "./contexts/globalAuthorizedContext";
-
-export { useAuth } from './hooks/loginForm/useAuth';
-export { useRootStore } from "./hooks/useRootStore";
-export { useIsMobile } from "./hooks/useIsMobile";
-export { useDebounce } from "./hooks/useDebounce";
-export { usePaging, usePagingWithController } from "./hooks/usePaging";
-export { useDateHelpers } from "./hooks/useDateHelpers";
-export { useNotifier } from "./hooks/useNotifier";
-export { LocalStorageHelpers } from "./hooks/localStorageHelpers";
-export { useApi } from "./hooks/useApi";
-export { useUpdate, useUpdateWithController } from "./hooks/useUpdate";
-export { useHash } from "./hooks/useHash";
-export { useClaims } from "./hooks/useClaims";
-export { useAutoSetInitialState } from "./hooks/useAutoSetInitialState";
-export { useSequentialPromises } from "./hooks/useSequentialPromises";
-export { useCopyToClipboard } from "./hooks/useCopyToClipboard";
-export { useRequestHandler } from './hooks/useRequestHandler';
-export { useColorSchemeStyles } from "./hooks/useColorSchemeStyles";
-export { useForkedRef, assignRef, isFunction } from "./hooks/useForkedRef";
-export { useDidMountEffect } from "./hooks/useDidMountEffect";
-export { useElementSize } from "./hooks/useElementSize";
-export { useLocalStorage } from "./hooks/useLocalStorage";
-export { useIsomorphicLayoutEffect } from "./hooks/useIsomorphicLayoutEffect";
-export { useEventListener } from "./hooks/useEventListener";
-export { useOutsideClick } from "./hooks/useOutsideClick";
-export { useMediaRecorder } from "./hooks/useMediaRecorder";
-export { useEditable } from "./hooks/useEditable";
-export { useIsFirstRender } from "./hooks/useIsFirstRender";
-export { useEventCallback } from "./hooks/useEventCallback";
-export { useCommittedRef } from "./hooks/useCommittedRef";
-export { useSound } from "./hooks/useSound";
-
-export { useTheme } from "./hooks/useTheme";
-export { useThrottle } from "./hooks/useThrottle";
-export { rootStore, RootStore } from "./stores/rootStore";
-
-
-// context
-export { RootStoreContext } from "./contexts/rootStoreContext";
-export { StoreContext, RootStoreProvider } from "./contexts/storeContext";
-export { GlobalAuthorizedContext, GlobalAuthorizedContextProvider } from "./contexts/globalAuthorizedContext";
-
-
-export type { PagingInfo } from "./helpers/types/PagingInfo";
-export type { IUseMediaRecorder, TMediaRecorderErrorTyp, TMediaRecorderStatus } from "./helpers/types/MediaRecorder";
-export type { NotificationType } from "./helpers/types";
-export type { IUseTimer } from "./helpers/types/Timer"
-export type { AssignableRef } from "./helpers/types/AssignableRef";
+// import {RootStoreProvider, StoreContext} from "./contexts/storeContext";
+// import {GlobalAuthorizedContext, GlobalAuthorizedContextProvider} from "./contexts/globalAuthorizedContext";
+//
+// export { useAuth } from './hooks/loginForm/useAuth';
+// export { useRootStore } from "./hooks/useRootStore";
+// export { useIsMobile } from "./hooks/useIsMobile";
+// export { useDebounce } from "./hooks/useDebounce";
+// export { usePaging, usePagingWithController } from "./hooks/usePaging";
+// export { useDateHelpers } from "./hooks/useDateHelpers";
+// export { useNotifier } from "./hooks/useNotifier";
+// export { LocalStorageHelpers } from "./hooks/localStorageHelpers";
+// export { useApi } from "./hooks/useApi";
+// export { useUpdate, useUpdateWithController } from "./hooks/useUpdate";
+// export { useHash } from "./hooks/useHash";
+// export { useClaims } from "./hooks/useClaims";
+// export { useAutoSetInitialState } from "./hooks/useAutoSetInitialState";
+// export { useSequentialPromises } from "./hooks/useSequentialPromises";
+// export { useCopyToClipboard } from "./hooks/useCopyToClipboard";
+// export { useRequestHandler } from './hooks/useRequestHandler';
+// export { useColorSchemeStyles } from "./hooks/useColorSchemeStyles";
+// export { useForkedRef, assignRef, isFunction } from "./hooks/useForkedRef";
+// export { useDidMountEffect } from "./hooks/useDidMountEffect";
+// export { useElementSize } from "./hooks/useElementSize";
+// export { useLocalStorage } from "./hooks/useLocalStorage";
+// export { useIsomorphicLayoutEffect } from "./hooks/useIsomorphicLayoutEffect";
+// export { useEventListener } from "./hooks/useEventListener";
+// export { useOutsideClick } from "./hooks/useOutsideClick";
+// export { useMediaRecorder } from "./hooks/useMediaRecorder";
+// export { useEditable } from "./hooks/useEditable";
+// export { useIsFirstRender } from "./hooks/useIsFirstRender";
+// export { useEventCallback } from "./hooks/useEventCallback";
+// export { useCommittedRef } from "./hooks/useCommittedRef";
+// export { useSound } from "./hooks/useSound";
+//
+// export { useTheme } from "./hooks/useTheme";
+// export { useThrottle } from "./hooks/useThrottle";
+// export { rootStore, RootStore } from "./stores/rootStore";
+//
+//
+// // context
+// export { RootStoreContext } from "./contexts/rootStoreContext";
+// export { StoreContext, RootStoreProvider } from "./contexts/storeContext";
+// export { GlobalAuthorizedContext, GlobalAuthorizedContextProvider } from "./contexts/globalAuthorizedContext";
+//
+//
+// export type { PagingInfo } from "./helpers/types/PagingInfo";
+// export type { IUseMediaRecorder, TMediaRecorderErrorTyp, TMediaRecorderStatus } from "./helpers/types/MediaRecorder";
+// export type { NotificationType } from "./helpers/types";
+// export type { IUseTimer } from "./helpers/types/Timer"
+// export type { AssignableRef } from "./helpers/types/AssignableRef";
 
 
 export { authorizeWithCredentials } from "./utils/auth";
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,3 +1,5 @@
+import { api } from "../services";
+
 export const authorizeWithCredentials = async ({
   authModel,
   setInviteCode,
