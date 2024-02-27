57301444e73d2e1435b19881703745c33822f22f
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-02-27 13:24:09 +0300

    update

--- hooks/useMediaRecorder.ts
+++ hooks/useMediaRecorder.ts
@@ -1,34 +1,6 @@
 import { useEffect, useRef, useState } from "react";
+import { IUseMediaRecorder, TMediaRecorderErrorType, TMediaRecorderStatus } from "../types";
 
-export interface IUseMediaRecorder {
-  recordingTime: number;
-  recordingBlob: Blob | null;
-  recordingAudioURL: string | null;
-  // recordingState?: RecordingState;
-  recorderStatus: TMediaRecorderStatus;
-  recordingStream?: MediaStream;
-  recordingTrack?: MediaStreamTrack | null;
-  recorderStart: () => void;
-  recorderPause: () => void;
-  recorderResume: () => void;
-  recorderStop: () => void;
-}
-
-export enum TMediaRecorderErrorType {
-  "NOT_SUPPORTED",
-  "NO_PERMISSION",
-}
-
-export type TMediaRecorderStatus =
-  | "idle"
-  | "loading"
-  | "acquiring_media"
-  | "ready"
-  | "recording"
-  | "paused"
-  | "stopping"
-  | "stopped"
-  | "failed";
 
 interface IUseMediaRecorderProps {
   onStart?: () => void;
--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,7 @@
 import {IBoardRelationSwitch, RelationsTypes} from "./BoardRelationSwitch.interface";
 
 export type { PagingInfo } from "./PagingInfo";
-export type { IUseMediaRecorder, TMediaRecorderErrorTyp, TMediaRecorderStatus } from "./MediaRecorder";
+export type { IUseMediaRecorder, TMediaRecorderErrorType, TMediaRecorderStatus } from "./MediaRecorder";
 export type { NotificationType } from ".";
 export type { IUseTimer } from "./Timer"
 export type { AssignableRef } from "./AssignableRef";
