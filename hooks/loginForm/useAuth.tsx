5413aeb04c993f822d90f8c706c0df021a985c69
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-02-28 14:47:24 +0300

    update

--- hooks/loginForm/useAuth.tsx
+++ hooks/loginForm/useAuth.tsx
@@ -29,6 +29,7 @@ export const useAuth = () => {
     console.log('authReq', authReq);
     if (authReq === true) {
       navigate("/orgchart");
+      console.log('notifier', notifier);
       notifier.show({ message: t("notifier:success.good_login"), theme: "success" });
     } else {
       setIsLoading(false);
