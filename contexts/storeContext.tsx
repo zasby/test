2101a63887fe07c37b5fd90ed1402eeb6e6c6648
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-02-28 17:12:40 +0300

    update

--- contexts/storeContext.tsx
+++ contexts/storeContext.tsx
@@ -6,9 +6,9 @@ let store: RootStore;
 export const StoreContext = createContext<RootStore | undefined>(undefined);
 
 export function RootStoreProvider({ children }: { children: ReactNode }) {
-  console.log('store', store);
-  console.log('new RootStore()', new RootStore());
+  console.log('RootStoreProvider store', store);
+  console.log('RootStoreProvider new RootStore()', new RootStore());
   const root = store ?? new RootStore();
-  console.log('root', root);
+  console.log('RootStoreProvider root', root);
   return <StoreContext.Provider value={root}>{children}</StoreContext.Provider>;
 }
