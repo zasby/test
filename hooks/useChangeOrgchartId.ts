38a9016cfabb461a20eb322ffc305dd25205c86c
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-10-02 15:25:55 +0300

    update

--- hooks/useChangeOrgchartId.ts
+++ hooks/useChangeOrgchartId.ts
@@ -1,13 +1,15 @@
-import { useCallback } from "react";
+import { useState } from "react";
 
 import { useRootStore } from "./useRootStore";
 
 export const useChangeOrgchartId = () => {
   const { orgchartStore } = useRootStore();
+  const [orgchartId, setOrgchartId] = useState<number | null>(null);
 
   const handleChangeOrgchartId = (orgchartId: number) => {
     console.log('handleChangeOrgchartId ', orgchartId);
     orgchartStore.setCurrentOrgchartId(orgchartId);
+    setOrgchartId(orgchartId);
     console.log('handleChangeOrgchartId getCurrentOrgchartId', orgchartStore.getCurrentOrgchartId);
 
   };
