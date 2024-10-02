9e4971766a4a51e1ffce130555b6e7162f6cae31
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-10-02 12:54:47 +0300

    update

--- hooks/useRootStore.ts
+++ hooks/useRootStore.ts
@@ -1,30 +1,12 @@
 import { useContext } from "react";
-import { RootStoreContext } from "../contexts";
-import { globalStore } from "../stores/globalStore";
+import { RootStoreContext } from "../contexts/rootStoreContext";
 
 export function useRootStore() {
-  // const context = useContext(RootStoreContext);
-  //
-  // if (context === undefined) {
-  //   throw new Error("useRootStore must be used within it's context provider");
-  // }
+  const context = useContext(RootStoreContext);
+  console.log("context", context);
+  if (context === undefined) {
+    throw new Error("useRootStore must be used within it's context provider");
+  }
 
-  console.log('useContext(RootStoreContext)', useContext(RootStoreContext));
-  console.log('globalStore.getStories ', globalStore.getStories);
-
-  // return globalStore.getStories ?? useContext(RootStoreContext);
-  return useContext(RootStoreContext);
+  return context;
 }
-
-// import { useContext } from "react";
-// import { RootStoreContext } from "../contexts/rootStoreContext";
-//
-// export function useRootStore() {
-//   const context = useContext(RootStoreContext);
-//   console.log('useRootStore context', context);
-//   if (context === undefined) {
-//     throw new Error("useRootStore must be used within it's context provider");
-//   }
-//
-//   return context;
-// }
