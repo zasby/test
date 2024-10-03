724514556488e5d16302ad4ef787d65495b4f01e
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-10-03 13:42:46 +0300

    update

--- wrapper/LayoutWrapper.tsx
+++ wrapper/LayoutWrapper.tsx
@@ -2,13 +2,17 @@ import React, { ReactNode } from 'react';
 import { rootStore } from "../stores/rootStore";
 import { RootStoreContext } from "../contexts/rootStoreContext";
 import { RootStoreProvider } from "../contexts/storeContext";
+import {Component} from "./component";
 
 export const LayoutWrapper = ({ children }: { children: ReactNode }) => (
   <RootStoreContext.Provider value={rootStore}>
     <>
       {console.log('children', children)}
     <RootStoreProvider>
+      <>
+        <Component/>
       { children }
+      </>
     </RootStoreProvider>
       </>
   </RootStoreContext.Provider>
--- wrapper/component.tsx
+++ wrapper/component.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRootStore } from "../hooks/useRootStore"
 
-const Component = () => {
+export const Component = () => {
   const {  orgchartStore } = useRootStore();
   console.log('Component getCurrentOrgchartId', orgchartStore.getCurrentOrgchartId);
   return <div>Component</div>;
