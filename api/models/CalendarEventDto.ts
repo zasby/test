3ff14d77e3a8e80079f83a62232d8968cf387f81
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-10-29 11:10:56 +0300

    TK-1175 fix

--- api/models/CalendarEventDto.ts
+++ api/models/CalendarEventDto.ts
@@ -3,11 +3,20 @@
 /* eslint-disable */
 
 import type { CalendarEventBorderDto } from './CalendarEventBorderDto';
+import { TagDto } from "./TagDto";
+import type { IssueCalculatedDto } from "./IssueCalculatedDto";
 
-type GoogleCalendarDTO = {
+export type GoogleCalendarDTO = {
     url?: string;
     creatorEmail: string;
     responseStatus: string;
+    internalGoogleEventStatus?: InternalGoogleEventStatus;
+}
+
+export enum InternalGoogleEventStatus {
+    Declined = "Declined", //Пользователь отказался от участия в данном событии,
+    Unfinished =  "Unfinished", //Событие еще не завершилось и пользователь не отказался от участия,
+    Finished = "Finished", //Событие завершено и пользователь не отказался от участия
 }
 
 export type CalendarEventDto = {
@@ -18,8 +27,17 @@ export type CalendarEventDto = {
     dateWorkStart?: string | null;
     timePlan?: string | null;
     timeFact?: string | null;
+    estimatedEndDate?: string | null;
+    dateDeadline?: string | null;
     userId?: number;
+    calculated?: IssueCalculatedDto;
     border?: CalendarEventBorderDto;
     isReadOnly? : boolean | false;
     googleCalendarData?: GoogleCalendarDTO;
+    isStrictDeadline?: boolean;
+    issueStatus?: string;
+    recurringIssueId?: number | null;
+    isManual?: boolean;
+    tags?: TagDto[];
+    internalGoogleEventStatus?: InternalGoogleEventStatus;
 }
