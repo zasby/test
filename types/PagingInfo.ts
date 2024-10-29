60af89d586968c83215c75f344e137ddef477388
Vitali Sharametsyeu <vitali.sharametsyeu@webfriends.by>
2024-10-29 11:27:40 +0300

    TK-1175 fix

--- types/PagingInfo.ts
+++ types/PagingInfo.ts
@@ -1,6 +1,10 @@
+import { UsersCount } from "../../api/models/CompanyForAdminDto";
+
 export type PagingInfo = {
   page: number;
   isLoading: boolean;
   isDone: boolean;
   totalItems?: number;
+  usersCount?: UsersCount;
+  timePlanTotal?: string;
 };
